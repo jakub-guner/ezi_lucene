9549189c709517cc9b1259bfd78291679d50fb16
Jakub Guner <jakub.guner@gmail.com>
2016-11-18 20:41:15 +0100

    first commit

---
+++ src/ir_course/LuceneSearchApp.java
@@ -0,0 +1,209 @@
+/*
+ * Skeleton class for the Lucene search program implementation
+ */
+package ir_course;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.document.DateTools.*;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LuceneSearchApp {
+
+	public static final String TITLE = "title";
+	public static final String DESCRIPTION = "description";
+	public static final String DATE = "date";
+
+	private static Directory ramDirectory = new RAMDirectory();
+
+	Path iPath = Paths.get("./rssIndex");
+
+	
+	public LuceneSearchApp() {
+
+	}
+	
+	public void index(List<RssFeedDocument> docs) throws IOException {
+		Directory fileDirectory = FSDirectory.open(iPath);
+		Analyzer analyzer = new StandardAnalyzer();
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+		indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+		IndexWriter indexWriter = new IndexWriter(fileDirectory, indexWriterConfig);
+
+		for(RssFeedDocument doc:docs){
+			Document document = new Document();
+
+			Field titleField = new StringField(TITLE, doc.getTitle(), Field.Store.YES);
+			document.add(titleField);
+			Field descField = new StringField(DESCRIPTION,doc.getDescription(), Field.Store.YES);
+			document.add(descField);
+			Field dateField = new StringField(DATE,DateTools.dateToString(doc.getPubDate(), Resolution.DAY), Field.Store.YES);
+			document.add(dateField);
+
+			indexWriter.addDocument(document);
+		}
+
+		indexWriter.close();
+	}
+	
+	public List<String> search(List<String> inTitle, List<String> notInTitle, List<String> inDescription, List<String> notInDescription, String startDate, String endDate) throws IOException {
+		Directory fileDirectory = FSDirectory.open(iPath);
+		printQuery(inTitle, notInTitle, inDescription, notInDescription, startDate, endDate);
+		IndexReader indexReader = DirectoryReader.open(fileDirectory);
+		IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+//		System.out.println("In index: "+indexReader.numDocs());
+
+		Analyzer analyzer = new StandardAnalyzer();
+		QueryParser queryParser = new QueryParser(TITLE, analyzer);
+
+		List<String> results = new LinkedList<String>();
+
+		try {
+			String queryString=inTitle.get(0);
+			System.out.println(queryString);
+			Query query = queryParser.parse("\""+queryString+"\"");
+			TopDocs hits = indexSearcher.search(query, 5);
+			System.out.println("Found: "+hits.totalHits);
+			for(ScoreDoc sc: hits.scoreDocs){
+				String title=indexSearcher.doc(sc.doc).get(TITLE);
+				results.add(title);
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		// implement the Lucene search here
+
+		return results;
+	}
+	
+	public void printQuery(List<String> inTitle, List<String> notInTitle, List<String> inDescription, List<String> notInDescription, String startDate, String endDate) {
+		System.out.print("Search (");
+		if (inTitle != null) {
+			System.out.print("in title: "+inTitle);
+			if (notInTitle != null || inDescription != null || notInDescription != null || startDate != null || endDate != null)
+				System.out.print("; ");
+		}
+		if (notInTitle != null) {
+			System.out.print("not in title: "+notInTitle);
+			if (inDescription != null || notInDescription != null || startDate != null || endDate != null)
+				System.out.print("; ");
+		}
+		if (inDescription != null) {
+			System.out.print("in description: "+inDescription);
+			if (notInDescription != null || startDate != null || endDate != null)
+				System.out.print("; ");
+		}
+		if (notInDescription != null) {
+			System.out.print("not in description: "+notInDescription);
+			if (startDate != null || endDate != null)
+				System.out.print("; ");
+		}
+		if (startDate != null) {
+			System.out.print("startDate: "+startDate);
+			if (endDate != null)
+				System.out.print("; ");
+		}
+		if (endDate != null)
+			System.out.print("endDate: "+endDate);
+		System.out.println("):");
+	}
+	
+	public void printResults(List<String> results) {
+		if (results.size() > 0) {
+			Collections.sort(results);
+			for (int i=0; i<results.size(); i++)
+				System.out.println(" " + (i+1) + ". " + results.get(i));
+		}
+		else
+			System.out.println(" no results");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length > 0) {
+			LuceneSearchApp engine = new LuceneSearchApp();
+			
+			RssFeedParser parser = new RssFeedParser();
+			parser.parse(args[0]);
+			List<RssFeedDocument> docs = parser.getDocuments();
+			
+			engine.index(docs);
+
+			List<String> inTitle;
+			List<String> notInTitle;
+			List<String> inDescription;
+			List<String> notInDescription;
+			List<String> results;
+			
+			// 1) search documents with words "kim" and "korea" in the title
+			inTitle = new LinkedList<String>();
+			inTitle.add("kim");
+			inTitle.add("korea");
+			results = engine.search(inTitle, null, null, null, null, null);
+			engine.printResults(results);
+			
+			// 2) search documents with word "kim" in the title and no word "korea" in the description
+			inTitle = new LinkedList<String>();
+			notInDescription = new LinkedList<String>();
+			inTitle.add("kim");
+			notInDescription.add("korea");
+			results = engine.search(inTitle, null, null, notInDescription, null, null);
+			engine.printResults(results);
+
+			// 3) search documents with word "us" in the title, no word "dawn" in the title and word "" and "" in the description
+			inTitle = new LinkedList<String>();
+			inTitle.add("us");
+			notInTitle = new LinkedList<String>();
+			notInTitle.add("dawn");
+			inDescription = new LinkedList<String>();
+			inDescription.add("american");
+			inDescription.add("confession");
+			results = engine.search(inTitle, notInTitle, inDescription, null, null, null);
+			engine.printResults(results);
+			
+			// 4) search documents whose publication date is 2011-12-18
+			results = engine.search(null, null, null, null, "2011-12-18", "2011-12-18");
+			engine.printResults(results);
+			
+			// 5) search documents with word "video" in the title whose publication date is 2000-01-01 or later
+			inTitle = new LinkedList<String>();
+			inTitle.add("video");
+			results = engine.search(inTitle, null, null, null, "2000-01-01", null);
+			engine.printResults(results);
+			
+			// 6) search documents with no word "canada" or "iraq" or "israel" in the description whose publication date is 2011-12-18 or earlier
+			notInDescription = new LinkedList<String>();
+			notInDescription.add("canada");
+			notInDescription.add("iraq");
+			notInDescription.add("israel");
+			results = engine.search(null, null, null, notInDescription, null, "2011-12-18");
+			engine.printResults(results);
+		}
+		else
+			System.out.println("ERROR: the path of a RSS Feed file has to be passed as a command line argument.");
+	}
+}
